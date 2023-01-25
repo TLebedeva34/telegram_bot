848f76a1455e90756a1a6f06e49e29bc3e98d899
TLebedeva34 <tlebedeva.34@gmail.com>
2023-01-25 17:14:50 +0300

    Changed files added

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '5911933111:AAEVW3mrXZYSfqheNP4hsAYmC8W7W8Mwc5o'
+
+keys = {
+    'рубль': 'RUB',
+    'доллар': 'EUR',
+    'евро': 'USD'
+}
--- extensions.py
+++ extensions.py
@@ -1,2 +1,37 @@
-a = "test"
-print(a)
+import requests
+import json
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+class Converter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}. Введите целое число.')
+
+        try:
+            amount = str(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}. Введите целое число.')
+
+        r = requests.get(f'https://v6.exchangerate-api.com/v6/fc740ee612b30099c9c461c9/pair/{quote_ticker}/{base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
